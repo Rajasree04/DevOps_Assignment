0cf7e20d9bb4b7b5f71b590a2eeaf8e7c9d672b8
Avvari Rajasree <avvari.r@HCL.COM>
2018-09-28 04:00:26 -0700

    Modified

--- Scripts/archiveCore.sh
+++ Scripts/archiveCore.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo 
+echo " This file is modified" 
 date
 for i in {004..013}; do
 sshpass -p '4o-Tx_B72f+RzL5*E6$qe!F3{K9tb8}J2%Sm/' ssh -qT -o StrictHostKeyChecking=no qvxpadm@prdqvxp${i} "/apps/qvxp/domains/qvhsdomain/qvhsinst{i}/config/OHS/qvhscomp{i}/gzip core.*" 
